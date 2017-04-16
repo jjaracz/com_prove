4e0a44c5b5ed96ed79f41002856d762381937373
Janek <j.jaracz@poczta.pl>
2017-04-16 20:41:02 +0200

    Fix display emails on the front page

--- site/prove.php
+++ site/prove.php
@@ -9,11 +9,6 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-if (!JFactory::getUser()->authorise('core.manage', 'com_prove'))
-{
-	throw new JAccessExceptionNotallowed(JText::_('JERROR_ALERTNOAUTHOR'), 403);
-}
-
 $controller = JControllerLegacy::getInstance('Prove');
 $controller->execute(JFactory::getApplication()->input->get('task'));
 $controller->redirect();
